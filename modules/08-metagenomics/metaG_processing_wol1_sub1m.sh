506b8148b70a83dfe0ea91802e8fb4f3f4ae0d52
Juan Tibocha <jdtibochab@gmail.com>
2024-09-12 15:28:53 -0700

    Updated script for metaG

--- modules/08-metagenomics/metaG_processing_wol1_sub1m.sh
+++ modules/08-metagenomics/metaG_processing_wol1_sub1m.sh
@@ -6,7 +6,7 @@
 
 conda activate bootcamp
 
-
+mkdir metag_tutorial_out
 echo "This is runnning woltka first step against the above50 database (large)" 
 ##############################################
 # Step 1. Data Trimming 
@@ -16,7 +16,7 @@ trim_galore \
 	--paired ./rawdata_sub1m/MI10_metaG_R1.fastq.gz ./rawdata_sub1m/MI10_metaG_R2.fastq.gz \
 	--quality 20 \
 	--fastqc \
-	-o ../metag_tutorial_out/trimmed
+	-o ./metag_tutorial_out/trimmed
 
 # 2 min
 
@@ -24,13 +24,13 @@ trim_galore \
 # Step 2. Data Alignment 
 ##############################################
 
-mkdir ../metag_tutorial_out/aligned
-mkdir ../metag_tutorial_out/aligned/samfiles
-mkdir ../metag_tutorial_out/aligned/bowfiles
+mkdir ./metag_tutorial_out/aligned
+mkdir ./metag_tutorial_out/aligned/samfiles
+mkdir ./metag_tutorial_out/aligned/bowfiles
 
 # use subset database
 
-bowtie2 -1 ../metag_tutorial_out/trimmed/MI10_metaG_R1_val_1.fq.gz -2 ../metag_tutorial_out/trimmed/MI10_metaG_R2_val_2.fq.gz -x ./reference_index_above10/mi01-12_above10_l -p 4 --no-unal --no-head -S ../metag_tutorial_out/aligned/samfiles/MI_10_metaG.sam 2> ../metag_tutorial_out/aligned/bowfiles/MI_10_metaG.bow
+bowtie2 -1 ./metag_tutorial_out/trimmed/MI10_metaG_R1_val_1.fq.gz -2 ./metag_tutorial_out/trimmed/MI10_metaG_R2_val_2.fq.gz -x ./reference_index_above10/mi01-12_above10_l -p 4 --no-unal --no-head -S ./metag_tutorial_out/aligned/samfiles/MI_10_metaG.sam 2> ./metag_tutorial_out/aligned/bowfiles/MI_10_metaG.bow
 
 # 1 min
 
@@ -38,11 +38,11 @@ bowtie2 -1 ../metag_tutorial_out/trimmed/MI10_metaG_R1_val_1.fq.gz -2 ../metag_t
 # Step 3. Extract features
 ##############################################
 
-mkdir ../metag_tutorial_out/features
+mkdir ./metag_tutorial_out/features
 
 woltka classify \
--i ../metag_tutorial_out/aligned/samfiles \
--o ../metag_tutorial_out/features/MI_10_metaG.tsv
+-i ./metag_tutorial_out/aligned/samfiles \
+-o ./metag_tutorial_out/features/MI_10_metaG.tsv
 
 # ~ 1 min
 
@@ -51,3 +51,4 @@ woltka classify \
 ##############################################
 
 conda deactivate
+
